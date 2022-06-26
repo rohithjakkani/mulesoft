22207d519f14544d3e53580737669a72ad947c7c
rohithjakkani <92290877+rohithjakkani@users.noreply.github.com>
2022-06-26 21:41:35 +0530

    Update CreateDbmovies.java

--- CreateDbmovies.java
+++ CreateDbmovies.java
@@ -2,29 +2,29 @@ package com.company;
 
 import java.sql.*;
 
-public class CreateMoviesDB {
+public class CreateMoviesDB{
     public static void main(String[] args) {
 
         try {
 
-            Connection connection = null;
-            Statement statement = null;
+            Connection conn = null;
+            Statement stmt = null;
 
-            connection = DriverManager
+            conn = DriverManager
                     .getConnection("jdbc:sqlite:/C:\\sqlite\\java\\connect\\net\\sqlitetutorial\\Mulesoft\\MoviesDatabase.db");
 
             System.out.println("Database Connection Successful !!");
 
-            statement = connection.createStatement();
+            stmt = conn.createStatement();
             String qry = "CREATE TABLE MyMovies " + "(MovieName TEXT NOT NULL,"
                     + " ActorName TEXT NOT NULL, " + " ActressName TEXT NOT NULL, "
                     + " ReleaseYear INTEGER NOT NULL, " + " DirectorName TEXT NOT NULL )";
 
-            statement.executeUpdate(qry);
+            stmt.executeUpdate(qry);
 
 
-            statement.close();
-            connection.close();
+            stmt.close();
+            conn.close();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
